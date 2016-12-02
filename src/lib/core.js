0e2971996c38ddce39c09033060d62cdef65c4b4
rogerbf <roger@bergfalk.nu>
2016-12-02 18:56:52 +0100

    catch correctly

--- src/lib/core.js
+++ src/lib/core.js
@@ -4,17 +4,20 @@ function pidcomm () {
   return new Promise((resolve, reject) => {
     exec('ps', ['-a', '-x', '-o', 'pid', '-o', 'comm'])
       .then(stdio => {
-        resolve(stdio.stdout
-          .split('\n')
-          .slice(1) // Skip headers
-          .map(line => line.trim())
-          .map(line => {
-            const pid = line.split(' ', 1).join()
-            const comm = line.split(' ').slice(1).join(' ')
-            return { pid, comm}
-          }))
-          .catch(e => reject(e))
+        resolve(
+          stdio.stdout
+            .split('\n')
+            .slice(1) // Skip headers
+            .map(line => line.trim())
+            .map(line => {
+              const pid = line.split(' ', 1).join()
+              const comm = line.split(' ').slice(1).join(' ')
+              return { pid, comm}
+            }
+          )
+        )
       })
+      .catch(e => reject(e))
   })
 }
 
